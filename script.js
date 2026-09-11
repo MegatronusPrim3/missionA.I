1459f8d6e7df457bb8c8d642dd421406298325a1
MegatronusPrim3 <rodrigues.sousa.pedro@escola.pr.gov.br>
2026-09-11 11:00:30 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,19 +6,27 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
-        alternativas: [
-            {
-                texto: "Isso é assustador!",
-                afirmacao: "afirmacao"
-            },
-            {
-                texto: "Isso é maravilhoso!",
-                afirmacao: "afirmacao"
-            }           
-            
-        ]
+      enunciado:
+        "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas  e crises existenciais que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+      alternativas: [
+        {
+          texto: "Isso é assustador!",
+          afirmacao: [
+            "No início ficou com medo do que essa tecnologia pode fazer, pois sou  cagão. ",
+            "Achou assustador pensar na velocidade na qual eu não tenho capacidade pois o meu cerebro esta derretido, a tecnologia está avançando.",
+          ],
+        },
+        {
+          texto: "Isso é maravilhoso!",
+          afirmacao: [
+            "Quis saber como usar IA no seu dia a dia.",
+            "Foi atrás de vídeos, artigos e mais informaçõe sobre como utilizar essa tecnologia.",
+          ],
+        },
+      ],
     },
+  ];
+    
     {
         enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial (IA), uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre elaIA. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de tecnologia em sala de aula. Qual atitude você toma?",
         alternativas: [
